d3b05a9c59fb58ff9ca5bff00d1dac1d8e6079a5
Tejas <tejasbait7@gmail.com>
2026-08-08 19:52:05 +0530

    sting

---
+++ Data-Structures-and-Algorithms/02_Strings/01_Valid_Anagram/README.md
@@ -0,0 +1,3 @@
+# LeetCode- Valid Anagram
+## Question
+Given two strings `s` and `t`, check whether they contain the same characters with the same frequency.
---
+++ Data-Structures-and-Algorithms/02_Strings/01_Valid_Anagram/solution.py
@@ -0,0 +1,21 @@
+class solution:
+    def isAnagram(self, s, t):
+
+        if len(s) != len(t):
+            return False
+        count = {}
+        for char in s:
+            count[char] = count.get(char, 0) + 1
+        for char in t:
+            if char not in count:
+                return False
+            count[char] -= 1
+            if count[char] < 0:
+                return False
+        return True
+
+# Test
+obj = solution()
+s = "abcd"
+t = "dcba"
+print(obj.isAnagram(s, t))
