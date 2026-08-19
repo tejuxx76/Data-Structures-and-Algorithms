6c556420482a4a12ac8b93b79e33536dd1c39a52
Tejas <tejasbait7@gmail.com>
2026-08-19 18:42:23 +0530

    string20

---
+++ Data-Structures-and-Algorithms/02_Strings/20_Decode_String/READMe.md
@@ -0,0 +1,4 @@
+# LeetCode - Valid Parentheses
+
+## Question
+Given a string `s` containing just the characters `'('`, `')'`, `'{'`, `'}'`, `'['` and `']'`, determine if the input string is valid.
---
+++ Data-Structures-and-Algorithms/02_Strings/20_Decode_String/solution.py
@@ -0,0 +1,19 @@
+def isValid(s: str) -> bool:
+    stack = []
+    mapping = {")": "(", "}": "{", "]": "["}
+
+    for char in s:
+        if char in mapping:
+            top_element = stack.pop() if stack else '#'
+            if mapping[char] != top_element:
+                return False
+        else:
+            stack.append(char)
+
+    return not stack
+
+print(isValid("()"))
+print(isValid("()[]{}"))
+print(isValid("(]"))
+print(isValid("([)]"))
+print(isValid("{[]}"))
