0f4e18a199f400e9a329c2a1e4e1c8857fd75413
Tejas <tejasbait7@gmail.com>
2026-09-04 17:35:01 +0530

    tpoint

---
+++ Data-Structures-and-Algorithms/04_Two_Pointers/01_Two_Sum_II_Input_Array_Is_Sorted/README.md
@@ -0,0 +1,6 @@
+# LeetCode
+- Two Sum II - Input Array Is Sorted
+## Question
+Given a sorted array and a target, find two numbers whose sum is equal
+to the target.
+Return their 1-based indexes.
---
+++ Data-Structures-and-Algorithms/04_Two_Pointers/01_Two_Sum_II_Input_Array_Is_Sorted/solution.py
@@ -0,0 +1,14 @@
+numbers = list(map(int, input("Enter sorted numbers: ").split()))
+target = int(input("Enter target: "))
+
+left = 0
+right = len(numbers) - 1
+while left < right:
+    total = numbers[left] + numbers[right]
+    if total == target:
+        print("Indexes:", left + 1, right + 1)
+        break
+    elif total < target:
+        left += 1
+    else:
+        right -= 1
