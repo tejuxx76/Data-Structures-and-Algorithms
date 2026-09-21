703be3b78948c22b7b9a2d7f9637a03e51fecfbe
Tejas <tejasbait7@gmail.com>
2026-09-21 18:03:21 +0530

    tpoint

---
+++ Data-Structures-and-Algorithms/04_Two_Pointers/06_Sort_Array_By_Parity/README.md
@@ -0,0 +1,5 @@
+# LeetCode
+- Sort Array By Parity
+## Question
+
+Given an integer array, move all even numbers before all odd numbers.
---
+++ Data-Structures-and-Algorithms/04_Two_Pointers/06_Sort_Array_By_Parity/solution.py
@@ -0,0 +1,12 @@
+nums = list(map(int, input("Enter numbers: ").split()))
+left = 0
+right = len(nums) - 1
+while left < right:
+
+    if nums[left] % 2 != 0 and nums[right] % 2 == 0:
+        nums[left], nums[right] = nums[right], nums[left]
+    if nums[left] % 2 == 0:
+        left += 1
+    if nums[right] % 2 != 0:
+        right -= 1
+print("Sorted by parity:", nums)
